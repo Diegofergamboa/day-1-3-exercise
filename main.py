bbbda0056849338548233398aeaa915208d09829
Diegofergamboa <diegofergamboa@hotmail.es>
2021-02-27 14:28:54 +0000

    Finish debugging

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 # This is a especial functions that have the value that two differents strings can have.
 
 my_string = input('What is your string?: ')
-print(len(my_string))
+#This is important to know because when you are wrtting code, the bettter option is almost always be pragmatic and chunk the code.
+lenght = len(my_string)
+print(lenght)
 
 
 
