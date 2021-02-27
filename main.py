7f9ed30f95fb7ab87d5016e9eb85e1a8fdbf0d21
Diegofergamboa <diegofergamboa@hotmail.es>
2021-02-27 14:09:12 +0000

    Complete challenge

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #Write your code below this line 👇
+#A function that says the lenght that a string. 
 
+
+# print('Hello, welcome ' + input('What is your name?: ')).
+# This is a especial functions that have the value that two differents strings can have.
+
+my_string = input('What is your string?: ')
+print(len(my_string))
 
 
 
